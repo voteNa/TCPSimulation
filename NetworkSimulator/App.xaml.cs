aba6462dc895215b05474828e73b8667491a6212
Yuliya Protasevich <Protasevich.Yuliya@gmail.com>
2020-04-06 20:01:30 +0700

    Add project files.

---
+++ NetworkSimulator/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using NetworkSimulator.ViewModels;
+using NetworkSimulator.Views;
+
+namespace NetworkSimulator
+{
+    public class App : Application
+    {
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public override void OnFrameworkInitializationCompleted()
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                desktop.MainWindow = new MainWindow
+                {
+                    DataContext = new MainWindowViewModel(),
+                };
+            }
+
+            base.OnFrameworkInitializationCompleted();
+        }
+    }
+}
